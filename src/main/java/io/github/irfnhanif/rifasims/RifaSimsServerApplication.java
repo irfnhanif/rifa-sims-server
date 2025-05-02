c568089cf4b89adaa15195cdcd43b691fbed0827
irfnhanif <14.irfanh@gmail.com>
2025-05-02 10:30:37 +0700

    Prepare Rifa server  project

---
+++ src/main/java/io/github/irfnhanif/rifasims/RifaSimsServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.irfnhanif.rifasims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RifaSimsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RifaSimsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/irfnhanif/rifasims/RifaSimsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.irfnhanif.rifasims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifaSimsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
