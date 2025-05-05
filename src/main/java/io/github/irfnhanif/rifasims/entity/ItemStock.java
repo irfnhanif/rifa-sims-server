91de4c3111dbb1c4fe73a0cc8d100d3db7626912
irfnhanif <14.irfanh@gmail.com>
2025-05-05 20:51:26 +0700

    Add getters and setters for ItemStock entity attributes

--- src/main/java/io/github/irfnhanif/rifasims/entity/ItemStock.java
+++ src/main/java/io/github/irfnhanif/rifasims/entity/ItemStock.java
@@ -13,10 +13,43 @@ public class ItemStock {
 
     @ManyToOne
     @JoinColumn(name = "item_id")
+    @Column(nullable = false)
     private Item item;
 
     @Column(nullable = false)
     private int currentStock;
 
     private Integer threshold;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getCurrentStock() {
+        return currentStock;
+    }
+
+    public void setCurrentStock(int currentStock) {
+        this.currentStock = currentStock;
+    }
+
+    public Integer getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(Integer threshold) {
+        this.threshold = threshold;
+    }
 }
