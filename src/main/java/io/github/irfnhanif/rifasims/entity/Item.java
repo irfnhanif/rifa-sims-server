b6b2155a2fb7c9462718ba868573c65ca008ce87
irfnhanif <14.irfanh@gmail.com>
2025-05-05 20:08:09 +0700

    Add Item entity with UUID as primary key

---
+++ src/main/java/io/github/irfnhanif/rifasims/entity/Item.java
@@ -0,0 +1,53 @@
+package io.github.irfnhanif.rifasims.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String barcode;
+
+    private String description;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
