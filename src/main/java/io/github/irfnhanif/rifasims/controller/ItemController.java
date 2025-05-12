f608aaa2bbf8cc5a47f34179664b8ab73ca56659
irfnhanif <14.irfanh@gmail.com>
2025-05-12 19:25:36 +0700

    Add ItemController for managing Item entities

---
+++ src/main/java/io/github/irfnhanif/rifasims/controller/ItemController.java
@@ -0,0 +1,51 @@
+package io.github.irfnhanif.rifasims.controller;
+
+import io.github.irfnhanif.rifasims.entity.Item;
+import io.github.irfnhanif.rifasims.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @PreAuthorize("hasRole('OWNER')")
+    @PostMapping("")
+    public ResponseEntity<Item> createItem(@RequestBody Item item) {
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Item>> getAllItems(@RequestParam(required = false) String name) {
+
+    }
+
+    @GetMapping("{itemId}")
+    public ResponseEntity<Item> getItem(@PathVariable UUID itemId) {
+
+    }
+
+    @PreAuthorize("hasRole('OWNER')")
+    @PutMapping("/{itemId}")
+    public ResponseEntity<Item> updateItem(@PathVariable UUID itemId, @RequestBody Item item) {
+
+    }
+
+    @PreAuthorize("hasRole('OWNER')")
+    @DeleteMapping("/{itemId}")
+    public ResponseEntity<Item> deleteItem(@PathVariable UUID itemId) {
+
+    }
+}
