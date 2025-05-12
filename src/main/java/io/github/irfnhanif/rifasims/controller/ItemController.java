00a31ca5a342f54dc68f1ab2afcde38468cdffee
irfnhanif <14.irfanh@gmail.com>
2025-05-12 20:03:09 +0700

    Add endpoints for retrieving item history and items by barcode in ItemController

--- src/main/java/io/github/irfnhanif/rifasims/controller/ItemController.java
+++ src/main/java/io/github/irfnhanif/rifasims/controller/ItemController.java
@@ -1,12 +1,14 @@
 package io.github.irfnhanif.rifasims.controller;
 
 import io.github.irfnhanif.rifasims.entity.Item;
+import io.github.irfnhanif.rifasims.entity.StockChangeType;
 import io.github.irfnhanif.rifasims.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,8 +34,20 @@ public class ItemController {
 
     }
 
-    @GetMapping("{itemId}")
+    @GetMapping("/{itemId}")
     public ResponseEntity<Item> getItem(@PathVariable UUID itemId) {
+
+    }
+
+    @GetMapping("/{itemId}/history")
+    public ResponseEntity<List<Item>> getItemHistory(@PathVariable UUID itemId,
+                                                     @RequestParam(required = false) LocalDateTime startDate, @RequestParam(required = false) LocalDateTime endDate,
+                                                     @RequestParam(required = false) StockChangeType type) {
+
+    }
+
+    @GetMapping("/barcode/{barcode}")
+    public ResponseEntity<List<Item>> getItemByBarcode(@RequestParam String barcode) {
 
     }
 
