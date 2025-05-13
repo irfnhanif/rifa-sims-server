80630bc613b68ea876884685173f9b35fe028142
irfnhanif <14.irfanh@gmail.com>
2025-05-13 20:27:19 +0700

    Refactor ItemController to use ItemService and enhance API responses with APIResponse DTO

--- src/main/java/io/github/irfnhanif/rifasims/controller/ItemController.java
+++ src/main/java/io/github/irfnhanif/rifasims/controller/ItemController.java
@@ -1,9 +1,13 @@
 package io.github.irfnhanif.rifasims.controller;
 
+import io.github.irfnhanif.rifasims.dto.APIResponse;
 import io.github.irfnhanif.rifasims.entity.Item;
 import io.github.irfnhanif.rifasims.entity.StockChangeType;
-import io.github.irfnhanif.rifasims.repository.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.github.irfnhanif.rifasims.exception.BadRequestException;
+import io.github.irfnhanif.rifasims.exception.InternalServerException;
+import io.github.irfnhanif.rifasims.service.ItemService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -16,50 +20,91 @@ import java.util.UUID;
 @RequestMapping("/api/items")
 public class ItemController {
 
-    @Autowired
-    private ItemRepository itemRepository;
+    private ItemService itemService;
 
-    public ItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
     }
 
-    @PreAuthorize("hasRole('OWNER')")
+//    @PreAuthorize("hasRole('OWNER')")
     @PostMapping("")
-    public ResponseEntity<Item> createItem(@RequestBody Item item) {
-
+    public ResponseEntity<APIResponse<Item>> createItem(@Valid  @RequestBody Item item) {
+        try {
+            Item createdItem = itemService.createItem(item);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(new APIResponse<>(true, "Item created successfully", createdItem, null));
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
     }
 
     @GetMapping("")
-    public ResponseEntity<List<Item>> getAllItems(@RequestParam(required = false) String name) {
-
+    public ResponseEntity<APIResponse<List<Item>>> getAllItems(@RequestParam(required = false) String name,
+                                                               @RequestParam(required = false, defaultValue = "0") Integer page,
+                                                               @RequestParam(required = false, defaultValue = "10") Integer size) {
+        try {
+            List<Item> items = itemService.getAllItems(name, page, size);
+            return ResponseEntity.ok(new APIResponse<>(true, "Items retrieved successfully", items, null));
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
     }
 
     @GetMapping("/{itemId}")
-    public ResponseEntity<Item> getItem(@PathVariable UUID itemId) {
-
+    public ResponseEntity<APIResponse<Item>> getItem(@PathVariable UUID itemId) {
+        try {
+            Item item = itemService.getItemById(itemId);
+            return ResponseEntity.ok(new APIResponse<>(true, "Item retrieved successfully", item, null));
+        } catch (Exception e) {
+            throw new InternalServerException("Internal server error", e.getMessage());
+        }
     }
 
     @GetMapping("/{itemId}/history")
     public ResponseEntity<List<Item>> getItemHistory(@PathVariable UUID itemId,
                                                      @RequestParam(required = false) LocalDateTime startDate, @RequestParam(required = false) LocalDateTime endDate,
                                                      @RequestParam(required = false) StockChangeType type) {
+        try {
 
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
     }
 
     @GetMapping("/barcode/{barcode}")
-    public ResponseEntity<List<Item>> getItemByBarcode(@RequestParam String barcode) {
+    public ResponseEntity<APIResponse<List<Item>>> getItemByBarcode(@RequestParam String barcode) {
+        try {
+            if (barcode == null || barcode.isEmpty()) {
+                throw new BadRequestException("Barcode cannot be empty");
+            }
 
+            List<Item> items = itemService.getItemsByBarcode(barcode);
+            return ResponseEntity.ok(new APIResponse<>(true, "Items retrieved successfully", items, null));
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
     }
 
-    @PreAuthorize("hasRole('OWNER')")
+//    @PreAuthorize("hasRole('OWNER')")
     @PutMapping("/{itemId}")
-    public ResponseEntity<Item> updateItem(@PathVariable UUID itemId, @RequestBody Item item) {
-
+    public ResponseEntity<APIResponse<Item>> updateItem(@PathVariable UUID itemId, @Valid @RequestBody Item item) {
+        try {
+            Item updatedItem = itemService.updateItem(item);
+            return ResponseEntity.ok(new APIResponse<>(true, "Item updated successfully", updatedItem, null));
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
     }
 
-    @PreAuthorize("hasRole('OWNER')")
+//    @PreAuthorize("hasRole('OWNER')")
     @DeleteMapping("/{itemId}")
-    public ResponseEntity<Item> deleteItem(@PathVariable UUID itemId) {
-
+    public ResponseEntity<APIResponse<Void>> deleteItem(@PathVariable UUID itemId) {
+        try {
+            itemService.deleteItem(itemId);
+            return ResponseEntity.ok(new APIResponse<>(true, "Item deleted successfully", null, null));
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
     }
 }
