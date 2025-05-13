d1c76a824fd4cb9db1ed3177868c4fb95f9ce858
irfnhanif <14.irfanh@gmail.com>
2025-05-13 19:34:32 +0700

    Add ResourceNotFoundException for handling resource not found errors

---
+++ src/main/java/io/github/irfnhanif/rifasims/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.irfnhanif.rifasims.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
