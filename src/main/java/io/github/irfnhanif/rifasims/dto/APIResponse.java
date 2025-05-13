db115cb89a40b4aa6ff6aadec625b98477b44c5c
irfnhanif <14.irfanh@gmail.com>
2025-05-13 18:08:14 +0700

    Add APIResponse class for standardized API responses

---
+++ src/main/java/io/github/irfnhanif/rifasims/dto/APIResponse.java
@@ -0,0 +1,48 @@
+package io.github.irfnhanif.rifasims.dto;
+
+import java.util.List;
+
+public class APIResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+    private List<String> errors;
+
+    public APIResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }
+}
