32ff9c7e015e2369f39f2c5d737ed8fa9d7db391
irfnhanif <14.irfanh@gmail.com>
2025-05-13 19:59:38 +0700

    Add BadRequestException and InternalServerException for improved error handling

---
+++ src/main/java/io/github/irfnhanif/rifasims/exception/BadRequestException.java
@@ -0,0 +1,11 @@
+package io.github.irfnhanif.rifasims.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+    public BadRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
