4717842c1745d78082b42e3ebf5d53c98d1c5a6a
irfnhanif <14.irfanh@gmail.com>
2025-05-18 20:48:39 +0700

    Add pagination support to ItemStockRepository for item name search

--- src/main/java/io/github/irfnhanif/rifasims/repository/ItemStockRepository.java
+++ src/main/java/io/github/irfnhanif/rifasims/repository/ItemStockRepository.java
@@ -2,6 +2,8 @@ package io.github.irfnhanif.rifasims.repository;
 
 import io.github.irfnhanif.rifasims.entity.Item;
 import io.github.irfnhanif.rifasims.entity.ItemStock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +14,5 @@ public interface ItemStockRepository extends JpaRepository<ItemStock, UUID> {
     Optional<ItemStock> findByItem(Item item);
     List<ItemStock> findByCurrentStockLessThan(Integer threshold);
 
+    Page<ItemStock> findByItem_NameContaining(String itemName, Pageable pageable);
 }
