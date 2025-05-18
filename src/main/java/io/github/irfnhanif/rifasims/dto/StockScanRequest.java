45b7361dd6630d5d758db7ef1640731926040108
irfnhanif <14.irfanh@gmail.com>
2025-05-18 20:48:51 +0700

    Add StockScanRequest DTO to handle stock change requests

---
+++ src/main/java/io/github/irfnhanif/rifasims/dto/StockScanRequest.java
@@ -0,0 +1,8 @@
+package io.github.irfnhanif.rifasims.dto;
+
+import io.github.irfnhanif.rifasims.entity.StockChangeType;
+
+public class StockScanRequest {
+    private StockChangeType changeType;
+    private Integer amount;
+}
