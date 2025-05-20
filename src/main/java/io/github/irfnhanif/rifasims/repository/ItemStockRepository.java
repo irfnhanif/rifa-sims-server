d43b7d08d0bca9bfc3a6c99d7453bf5e916a1c6b
irfnhanif <14.irfanh@gmail.com>
2025-05-20 14:20:32 +0700

    Add query to retrieve ItemStocks by barcode with BarcodeScanResponse DTO

--- src/main/java/io/github/irfnhanif/rifasims/repository/ItemStockRepository.java
+++ src/main/java/io/github/irfnhanif/rifasims/repository/ItemStockRepository.java
@@ -1,5 +1,6 @@
 package io.github.irfnhanif.rifasims.repository;
 
+import io.github.irfnhanif.rifasims.dto.BarcodeScanResponse;
 import io.github.irfnhanif.rifasims.entity.Item;
 import io.github.irfnhanif.rifasims.entity.ItemStock;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,10 @@ import java.util.UUID;
 
 public interface ItemStockRepository extends JpaRepository<ItemStock, UUID> {
     Optional<ItemStock> findByItem(Item item);
+    @Query("SELECT new io.github.irfnhanif.rifasims.dto.BarcodeScanResponse(is.id, i.name) " +
+            "FROM Item i JOIN ItemStock is ON  i.id = is.item.id " +
+            "WHERE i.barcode = :barcode")
+    List<BarcodeScanResponse> findItemStocksByBarcode(String barcode);
 
     @Query("SELECT is FROM ItemStock is WHERE is.currentStock < is.threshold")
     Page<ItemStock> findItemStocksBelowThreshold(Pageable pageable);
