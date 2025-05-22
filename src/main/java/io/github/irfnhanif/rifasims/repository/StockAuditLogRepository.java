1c404884d3c340fc95ab373304ecae19833640af
irfnhanif <14.irfanh@gmail.com>
2025-05-22 13:24:03 +0700

    Enhance StockAuditLogRepository to support JPA specifications for improved query capabilities

--- src/main/java/io/github/irfnhanif/rifasims/repository/StockAuditLogRepository.java
+++ src/main/java/io/github/irfnhanif/rifasims/repository/StockAuditLogRepository.java
@@ -4,12 +4,13 @@ import io.github.irfnhanif.rifasims.entity.Item;
 import io.github.irfnhanif.rifasims.entity.StockAuditLog;
 import io.github.irfnhanif.rifasims.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public interface StockAuditLogRepository  extends JpaRepository<StockAuditLog, UUID> {
+public interface StockAuditLogRepository  extends JpaRepository<StockAuditLog, UUID>, JpaSpecificationExecutor<StockAuditLog> {
     List<StockAuditLog> findByItem(Item item);
     List<StockAuditLog> findByUser(User user);
     List<StockAuditLog> findByTimestampBetween(LocalDateTime start, LocalDateTime end);
