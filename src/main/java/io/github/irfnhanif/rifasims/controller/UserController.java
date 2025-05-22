c76a1ba6d88986bf3195e59eace8e84195120ab8
irfnhanif <14.irfanh@gmail.com>
2025-05-22 20:43:46 +0700

    Refactor UserController to use UserService and add endpoints for accepting and rejecting users

--- src/main/java/io/github/irfnhanif/rifasims/controller/UserController.java
+++ src/main/java/io/github/irfnhanif/rifasims/controller/UserController.java
@@ -1,63 +1,58 @@
 package io.github.irfnhanif.rifasims.controller;
 
+import io.github.irfnhanif.rifasims.dto.APIResponse;
 import io.github.irfnhanif.rifasims.entity.User;
 import io.github.irfnhanif.rifasims.entity.UserRole;
 import io.github.irfnhanif.rifasims.repository.UserRepository;
+import io.github.irfnhanif.rifasims.service.UserService;
+import jakarta.ws.rs.InternalServerErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @PreAuthorize("hasRole('OWNER')")
-    @PostMapping("")
-    public void createUser(@RequestBody User user) {
-
+    @GetMapping("/pending")
+    public ResponseEntity<APIResponse<List<User>>> getPendingUsers() {
+        try {
+            List<User> users = userService.getPendingUsers();
+            return ResponseEntity.ok(new APIResponse<>(true, "Pending users retrieved successfully", users, null));
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
     }
 
-    @PreAuthorize("hasRole('OWNER')")
-    @GetMapping("")
-    public ResponseEntity<Iterable<User>> getAllUsers(@RequestParam(required = false) Integer branch,
-                                                      @RequestParam(required = false)UserRole role) {
+    @PatchMapping("/{userId}/accept")
+    public ResponseEntity<APIResponse<String>> acceptUser(@PathVariable UUID userId) {
+        try {
+            User acceptedUser = userService.acceptUser(userId);
+            String response = String.format("User %s accepted successfully", acceptedUser.getUsername());
+            return ResponseEntity.ok(new APIResponse<>(true, "User accepted successfully", response, null));
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
     }
 
-    @PreAuthorize("hasRole('OWNER')")
-    @GetMapping("/{userId}")
-    public ResponseEntity<User> getUser(@PathVariable UUID userId) {
-
-    }
-
-    @PreAuthorize("hasRole('OWNER')")
-    @PutMapping("/{userId}")
-    public void updateUser(@PathVariable UUID userId, @RequestBody User user) {
-
-    }
-
-    @PreAuthorize("hasRole('OWNER')")
-    @DeleteMapping("/{userId}")
-    public void deleteUser(@PathVariable UUID userId) {
-
-    }
-
-    @GetMapping("/me")
-    public ResponseEntity<User> getCurrentUser() {
-
-    }
-
-    @PutMapping("/me")
-    public void updateCurrentUser(@RequestBody User user) {
-
+    @PatchMapping("/{userId}/reject")
+    public ResponseEntity<APIResponse<String>> rejectUser(@PathVariable UUID userId) {
+        try {
+            User rejectedUser = userService.rejectUser(userId);
+            String response = String.format("User %s rejected successfully", rejectedUser.getUsername());
+            return ResponseEntity.ok(new APIResponse<>(true, "User rejected successfully", response, null));
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
     }
 }
