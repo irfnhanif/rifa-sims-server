66d614154c5361c5ea5402c09f884d9ec0eaf847
irfnhanif <14.irfanh@gmail.com>
2025-05-22 20:43:39 +0700

    Refactor AuthController to use ResponseEntity for register and login methods

--- src/main/java/io/github/irfnhanif/rifasims/controller/AuthController.java
+++ src/main/java/io/github/irfnhanif/rifasims/controller/AuthController.java
@@ -5,6 +5,8 @@ import io.github.irfnhanif.rifasims.entity.User;
 import io.github.irfnhanif.rifasims.service.AuthService;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.InternalServerErrorException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,21 +23,21 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public APIResponse<String> register(@Valid @RequestBody User user) {
+    public ResponseEntity<APIResponse<String>> register(@Valid @RequestBody User user) {
         try {
             User registeredUser = authService.register(user);
             String registerMessage = String.format("Registered user: %s", registeredUser.getUsername());
-            return new APIResponse<>(true, "Register successfully, Please wait confirmation from owner", registerMessage, null);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new APIResponse<>(true, "Register successfully, Please wait confirmation from owner", registerMessage, null));
         } catch (Exception e) {
             throw new InternalServerErrorException(e.getMessage());
         }
     }
 
     @PostMapping("/login")
-    public APIResponse<String> login(@RequestBody User user) {
+    public ResponseEntity<APIResponse<String>> login(@RequestBody User user) {
         try{
             String token = authService.login(user.getUsername(), user.getPassword());
-            return new APIResponse<>(true, "Login successfully", token, null);
+            return ResponseEntity.ok(new APIResponse<>(true, "Login successfully", token, null));
         } catch (Exception e) {
             throw new InternalServerErrorException(e.getMessage());
         }
