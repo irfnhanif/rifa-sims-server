5be74ff65c8a5dadbcf56368d32ee919161ce8e7
irfnhanif <14.irfanh@gmail.com>
2025-05-22 20:42:15 +0700

    Add UserService for managing user status and retrieval of pending users

---
+++ src/main/java/io/github/irfnhanif/rifasims/service/UserService.java
@@ -0,0 +1,36 @@
+package io.github.irfnhanif.rifasims.service;
+
+import io.github.irfnhanif.rifasims.entity.User;
+import io.github.irfnhanif.rifasims.entity.UserStatus;
+import io.github.irfnhanif.rifasims.exception.ResourceNotFoundException;
+import io.github.irfnhanif.rifasims.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getPendingUsers() {
+        List<User> pendingUsers = userRepository.findByStatus(UserStatus.PENDING);
+        return pendingUsers;
+    }
+
+    public User acceptUser(UUID userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        user.setStatus(UserStatus.ACTIVE);
+        return userRepository.save(user);
+    }
+
+    public User rejectUser(UUID userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        user.setStatus(UserStatus.REJECTED);
+        return userRepository.save(user);
+    }
+}
